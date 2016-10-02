21ae1d1511f0cf8ee3e843c2e0a8d6ff08112bd1
ConceptCodes <conceptcodes@gmail.com>
2016-10-02 10:16:57 -0400

    Add files via upload

---
+++ Whatsforlunch/src/Whatsforlunch.java
@@ -0,0 +1,180 @@
+import java.util.Scanner;
+public class Whatsforlunch {
+
+	public static void main(String[] args) {
+		//Set up Scanner
+		Scanner name = new Scanner(System.in);
+		Scanner date = new Scanner(System.in);
+		
+		// Ask for name and store value as string
+		System.out.println("Enter your name");
+		System.out.print("> ");
+		String Name = name.nextLine();
+		
+		
+		// Ask for date and store date as int
+		System.out.println("Enter the date");
+		System.out.print("> ");
+		int Date = date.nextInt() ;
+		
+		// if statements
+		
+		
+		
+		
+		if (Date == 3)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Hot Roast beef sandwich");
+			
+		}
+		
+		
+		if (Date == 4)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Popcorn or buffalo chicken");
+			
+		}
+		
+		if (Date == 5)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Philly Cheese Steak");
+			
+		}
+		
+		if (Date == 6)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Pasta bar | Chicken Parmesan");
+			
+		}
+		
+		if (Date == 7)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Made to order Nachos");
+			
+		}
+		
+		
+		
+		
+		
+		if (Date == 11)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Chicken Tenders");
+			
+		}
+		
+		if (Date == 12)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Home made meatballs w/ pasta");
+			
+		}
+		
+		if (Date == 13)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Made to order wraps");
+			
+		}
+		
+		if (Date == 14)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Mozzarella stix w/ Garlic bread");
+			
+		}
+		
+		
+		
+		if (Date == 17)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Grilled flatbread sandwiches");
+			
+		}
+		
+		if (Date == 18)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Made to order wraps in a Large soft Tortilla");
+			
+		}
+		
+		if (Date == 19)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Pasta bar Chicken Alfredo w/ Garlic bread");
+			
+		}
+		
+		if (Date == 20)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Popcorn or Buffalo Chicken");
+			
+		}
+		
+		if (Date == 21)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Cheesesteak Calzone");
+			
+		}
+		
+		
+		if (Date == 24)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Made to order Wraps");
+			
+		}
+		
+		if (Date == 25)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Pasta Bar-Baked Ziti w/ Garlic bread");
+			
+		}
+		
+		if (Date == 26)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Chhicken Smackers w/ biscuit");
+			
+		}
+		
+		if (Date == 27)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Chicken Steak Sandwhich plain or w/ cheese");
+			
+		}
+		
+		if (Date == 28)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Hamburger, Cheese Burger or 2 Sloppy joe sliders");
+			
+		}
+		
+		
+		
+		if (Date == 31)
+		{
+			
+			System.out.println("Hello, " +Name +" Lunch is Hot Roast Beef w/ Gravy or Au Jus");
+			
+		}
+		
+
+	}
+	
+	
+	
+}
